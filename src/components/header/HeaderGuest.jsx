cf20bfd00f08bc825843caa4d8f0fdd8df6c6655
chkharto <dato.chkhartishvili24@gmail.com>
2026-04-16 22:55:03 +0400

    link

--- src/components/header/HeaderGuest.jsx
+++ src/components/header/HeaderGuest.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Sparkle, SparkleIcon, Sparkles, Stars } from 'lucide-react'
+import { Link } from 'react-router-dom'
 
 const HeaderGuest = ({ setLogin, setSignup }) => {
   return (
     <div className='flex items-center gap-9 text-[20px]'>
         <div className="flex items-center gap-2 text-text hover:text-primary cursor-pointer">
             <Sparkles />
-            <>Browse Courses</>
+            <Link to="/courses">Browse Courses</Link>
         </div>
         <div className="gap-10.75">
             <button onClick={() => setLogin(true)} className="px-3.5 py-2 border-2 border-[#958FEF] rounded-lg text-[#4F46E5] cursor-pointer">
